cd44bc380b4141685bf8640b45e6696d634c4625
Manuel Aguirre <aguirreman98@gmail.com>
2026-06-28 17:03:26 -0500

    updated the planets page to include the whole card to route to single page and also added some small framer motion animations to give it a nice touch of creativity

--- src/components/PlanetGrid.tsx
+++ src/components/PlanetGrid.tsx
@@ -1,12 +1,16 @@
+import { motion } from 'motion/react'
 import type { PlanetsResponse } from '@/types/types'
 import PlanetsCard from './PlanetsCard'
 
 export default function PlanetGrid({ planets }: { planets: PlanetsResponse }) {
+
   return (
-    <div className='grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3'>
-      {planets.items.map((planet) => (
-        <PlanetsCard key={planet.id} planet={planet} />
-      ))}
-    </div>
+    <motion.div hidden={planets.items.length === 0} initial={{ opacity: 0}} animate={{ opacity: 1 }}>
+      <div className='grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3'>
+        {planets.items.map((planet) => (
+          <PlanetsCard key={planet.id} planet={planet} />
+        ))}
+      </div>
+    </motion.div>
   )
 }
--- src/components/PlanetsCard.tsx
+++ src/components/PlanetsCard.tsx
@@ -1,14 +1,18 @@
 import type { Planets } from '@/types/types'
 import { Link } from 'react-router'
+import { motion } from 'motion/react'
+
 
 export default function PlanetsCard({ planet }: { planet: Planets }) {
   return (
-    <article className='bg-card shadow rounded-lg p-6'>
-      <h2 className='text-2xl font-bold text-secondary-foreground text-center mb-2'>{planet.name}</h2>
-      <img src={planet.image} alt={planet.name} className='mb-4 h-64 w-full rounded-lg object-cover' />
-      <Link to={`/planets/${planet.id}`} className='text-secondary-foreground hover:underline'>
-        View Planet
-      </Link>
-    </article>
+    <Link to={`/planets/${planet.id}`} className="block no-underline">
+      <motion.article whileHover={{ scale: 1.05, y: -5}} transition={{ duration: 0.2 }} className='bg-card cursor-pointer overflow-hidden shadow rounded-lg p-6'>
+        <h2 className='text-2xl font-bold text-secondary-foreground text-center mb-2'>{planet.name}</h2>
+        <img src={planet.image} alt={planet.name} className='mb-4 h-64 w-full rounded-lg object-cover' />
+        <span className='mt-auto inline-block text-primary font-semibold hover:underline text-center w-full'>
+          View Planet →
+        </span>
+      </motion.article>
+    </Link>
   )
 }
